48052a8a26c77e449d29eef6a51673ddb5b33668
Shrestha <shreshtha.tola_cs18@gla.ac.in>
2019-08-22 15:14:46 +0530

    accept input from the user

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String user_name = "";
+        String user_name = scanner.nextLine().trim();
         scanner.close();
 
 
