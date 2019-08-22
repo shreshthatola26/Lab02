2e60a37d3eaf4bcb8c2e2318639db9ef33c56a88
Shrestha <shreshtha.tola_cs18@gla.ac.in>
2019-08-22 15:16:19 +0530

    add output to the screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,10 +10,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String user_name = scanner.nextLine().trim();
+        System.out.println("Welcome," + user_name + "!");
         scanner.close();
-
-
-
     }
 }
 
